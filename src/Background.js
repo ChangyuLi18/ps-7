7111b04eaffb6d4344da857f11163888dc03a194
ChangyuLi18 <changl28@uw.edu>
2018-11-16 20:29:47 -0800

    first commit

---
+++ src/Background.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+export class Background extends Component {
+    render() {
+        return(
+            <div className="container">
+                <div>
+                    Major1: Informatics (Data Science)
+                </div>
+                <div>
+                    Major2: International Studies (Internatinoal Political Economics)
+                </div>
+            </div>
+    )}
+}
