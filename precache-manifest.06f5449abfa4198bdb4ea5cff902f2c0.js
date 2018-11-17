d45b629319f974036c7b471fd21f46ebd2b9b088
ChangyuLi18 <changl28@uw.edu>
2018-11-16 20:55:53 -0800

    Updates

---
+++ precache-manifest.06f5449abfa4198bdb4ea5cff902f2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f03bc24316902ef4786",
+    "url": "/ChangyuLi18/ps-7/static/js/runtime~main.7f03bc24.js"
+  },
+  {
+    "revision": "5d40939ae7d55eb20c18",
+    "url": "/ChangyuLi18/ps-7/static/js/main.5d40939a.chunk.js"
+  },
+  {
+    "revision": "1ff77a2f35d4272f7778",
+    "url": "/ChangyuLi18/ps-7/static/js/1.1ff77a2f.chunk.js"
+  },
+  {
+    "revision": "5d40939ae7d55eb20c18",
+    "url": "/ChangyuLi18/ps-7/static/css/main.9519da87.chunk.css"
+  },
+  {
+    "revision": "1ff77a2f35d4272f7778",
+    "url": "/ChangyuLi18/ps-7/static/css/1.da3602eb.chunk.css"
+  },
+  {
+    "revision": "52131cb30c87bf6c51b6d71b5d53f6ee",
+    "url": "/ChangyuLi18/ps-7/index.html"
+  }
+];
